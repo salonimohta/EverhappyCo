80231c773e5d8f8aeb8d48793848123c9c1c4df8
Saloni Mohta <salonimohta37@gmail.com>
2020-07-12 21:14:48 +0530

    Renames Carousel to ProductCarousel

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 //import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import ProductCarousel from '../Carousel';
+import ProductCarousel from '../ProductCarousel';
 
 export default class Home extends React.Component {
      render() {
