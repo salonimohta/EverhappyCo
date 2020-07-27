05eda40c85dca57551b56b35c90ebc05b7e67172
Saloni Mohta <salonimohta37@gmail.com>
2020-07-28 01:12:21 +0530

    Adds search mechanism using select in react-select

--- src/Search/index.js
+++ src/Search/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './index.css';
 import SearchIcon from '@material-ui/icons/Search';
+import productDetails from '../productDetails-json';
+import Select,{ components } from 'react-select';
+
+const skipWords=[];
+
+const searchList=productDetails.map(product=>{
+    return{
+        value: product.productCategory.replace(/_/g,' ')+' - '+product.productName.replace(/_/g,' ').replace(/\|/g,', '),
+        label: product.productCategory.replace(/_/g,' ')+' - '+product.productName.replace(/_/g,' ').replace(/\|/g,', ')
+    }
+});
 
 export default class Search extends React.Component{
     constructor(){
@@ -12,19 +23,58 @@ export default class Search extends React.Component{
     searchSpace=(event)=>{
         let keyword = event.target.value;
         this.setState({search:keyword})
-        console.log(keyword);
+    }
+    search(){
+        console.log(this.state.search);
     }
     render(){
-        return(
+        /*
+        const items = productDetails.filter((product)=>{
+            if(this.state.search == null)
+                return null;
+            else if(product.productName.replace('_',' ').toLowerCase().includes(this.state.search.toLowerCase()) || product.productCategory.replace('_',' ').toLowerCase().includes(this.state.search.toLowerCase())){
+                return product
+            }
+          }).map(product=>{
+            return(
             <div>
-                <form>
+              <ul>
+              <li>
+                    <span>{product.productName}</span>
+                    <span>{product.productCategory}</span>
+                </li>
+              </ul>
+            </div>
+            )
+          })
+          */
+          const DropdownIndicator = props => {
+            return (
+              <components.DropdownIndicator {...props}>
+                <SearchIcon />
+              </components.DropdownIndicator>
+            );
+          };
+          const onKeyDown = e => {
+              if (e.keyCode===13){
+                  this.searchSpace(e);
+                  this.search();
+                  alert(e.target.value)
+              }
+          }
+      
+        return(
+            <div style={{width:"300px"}}>
+                <Select placeholder="Search for products.." options={searchList} components={{DropdownIndicator}} onKeyDown={onKeyDown} />
+                {
+                    /*<form>
                     <div className="searchBar">
                     <SearchIcon fontSize={"large"} />
                     <input type="text" placeholder="Search for Products.." onChange={(e)=>this.searchSpace(e)}/> 
-                    <input type="submit" value="Search" />
-
+                    {items}
                     </div>
                 </form>
+                */}
             </div>
         )
     }
