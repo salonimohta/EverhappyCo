bc1da86f8c3216f212fd6f2b29a87bf727cda907
Saloni Mohta <salonimohta37@gmail.com>
2020-07-27 22:59:52 +0530

    Adds productDetails to a json file

--- src/Search/index.js
+++ src/Search/index.js
@@ -3,13 +3,24 @@ import './index.css';
 import SearchIcon from '@material-ui/icons/Search';
 
 export default class Search extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            search:null
+        };
+    }
+    searchSpace=(event)=>{
+        let keyword = event.target.value;
+        this.setState({search:keyword})
+        console.log(keyword);
+    }
     render(){
         return(
             <div>
                 <form>
                     <div className="searchBar">
                     <SearchIcon fontSize={"large"} />
-                    <input type="text" placeholder="Search for Products.." /> 
+                    <input type="text" placeholder="Search for Products.." onChange={(e)=>this.searchSpace(e)}/> 
                     <input type="submit" value="Search" />
 
                     </div>
