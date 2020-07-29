41e2cb95622205acc9b956f92d2d2c9ddcfd3720
Saloni Mohta <salonimohta37@gmail.com>
2020-07-29 23:18:40 +0530

    AboutUs content added

--- src/AboutUs/index.js
+++ src/AboutUs/index.js
@@ -5,8 +5,14 @@ import './index.css';
 export default class Home extends React.Component {
      render() {
          return (
-             <div class="welcomeHome">
-                 <b>Welcome to Everhappy Commercial Corporation</b>
+             <div class="aboutUsStyle">
+                 <p>Everhappy Commercial Corporation is a family run business conglomerate with a business history of more than 75 years with impeccable reputation of fair business dealings with all the stakeholders involved.</p>
+                
+                <p>ECC has traveled a long way to reach the scaling heights of success. Processing the ability to dream, the sprit to explore new opportunities and face new challenges the group made its dream a reality taking their ventures to greater heights.</p>
+                
+                <p>Hard work, Fair Dealing, Customer Satisfaction, and a Dynamic Perception, have been the guideline principal for the group. With these principals kept in mind, we are dealing in transformer materials, glass fibre products, mica products, furnace materials, packaging materials, and all electrical wires, cables and accessories. We are helping companies achieve their vision since 1964.</p>
+                
+                <p>We as a family run business believe in creating a relationship with all our customers, suppliers and employees as that of a large family and run a business based on trust, mutual respect, fairness and a win-win environment for everyone involved.</p>
              </div>
          )
      }
