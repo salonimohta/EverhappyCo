e1e2b297e00181a0664bf68277261deed5700ffa
Saloni Mohta <salonimohta37@gmail.com>
2020-07-30 00:33:32 +0530

    Redirects options in search-bar to respective pages

--- src/Search/index.js
+++ src/Search/index.js
@@ -3,51 +3,68 @@ import './index.css';
 import SearchIcon from '@material-ui/icons/Search';
 import productDetails from '../productDetails-json';
 import Select,{ components } from 'react-select';
+import Button from 'react-bootstrap/Button'
+import {withRouter} from 'react-router-dom';
 
-const skipWords=[];
+const skipWordsList=['a', 'able', 'about', 'above', 'abroad', 'according', 'accordingly', 'across', 'actually', 'adj', 'after', 'afterwards', 'again', 'against', 'ago', 'ahead', "ain't", 'all', 'allow', 'allows', 'almost', 'alone', 'along', 'alongside', 'already', 'also', 'although', 'always', 'am', 'amid', 'amidst', 'among', 'amongst', 'an', 'and', 'another', 'any', 'anybody', 'anyhow', 'anyone', 'anything', 'anyway', 'anyways', 'anywhere', 'apart', 'appear', 'appreciate', 'appropriate', 'are', "aren't", 'around', 'as', "a's", 'aside', 'ask', 'asking', 'associated', 'at', 'available', 'away', 'awfully', 'b', 'back', 'backward', 'backwards', 'be', 'became', 'because', 'become', 'becomes', 'becoming', 'been', 'before', 'beforehand', 'begin', 'behind', 'being', 'believe', 'below', 'beside', 'besides', 'best', 'better', 'between', 'beyond', 'both', 'brief', 'but', 'by', 'c', 'came', 'can', 'cannot', 'cant', "can't", 'caption', 'cause', 'causes', 'certain', 'certainly', 'changes', 'clearly', "c'mon", 'co', 'co.', 'com', 'come', 'comes', 'concerning', 'consequently', 'consider', 'considering', 'contain', 'containing', 'contains', 'corresponding', 'could', "couldn't", 'course', "c's", 'currently', 'd', 'dare', "daren't", 'definitely', 'described', 'despite', 'did', "didn't", 'different', 'directly', 'do', 'does', "doesn't", 'doing', 'done', "don't", 'down', 'downwards', 'during', 'e', 'each', 'edu', 'eg', 'eight', 'eighty', 'either', 'else', 'elsewhere', 'end', 'ending', 'enough', 'entirely', 'especially', 'et', 'etc', 'even', 'ever', 'evermore', 'every', 'everybody', 'everyone', 'everything', 'everywhere', 'ex', 'exactly', 'example', 'except', 'f', 'fairly', 'far', 'farther', 'few', 'fewer', 'fifth', 'first', 'five', 'followed', 'following', 'follows', 'for', 'forever', 'former', 'formerly', 'forth', 'forward', 'found', 'four', 'from', 'further', 'furthermore', 'g', 'get', 'gets', 'getting', 'given', 'gives', 'go', 'goes', 'going', 'gone', 'got', 'gotten', 'greetings', 'h', 'had', "hadn't", 'half', 'happens', 'hardly', 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", 'hello', 'help', 'hence', 'her', 'here', 'hereafter', 'hereby', 'herein', "here's", 'hereupon', 'hers', 'herself', "he's", 'hi', 'him', 'himself', 'his', 'hither', 'hopefully', 'how', 'howbeit', 'however', 'hundred', 'i', "i'd", 'ie', 'if', 'ignored', "i'll", "i'm", 'immediate', 'in', 'inasmuch', 'inc', 'inc.', 'indeed', 'indicate', 'indicated', 'indicates', 'inner', 'inside', 'insofar', 'instead', 'into', 'inward', 'is', "isn't", 'it', "it'd", "it'll", 'its', "it's", 'itself', "i've", 'j', 'just', 'k', 'keep', 'keeps', 'kept', 'know', 'known', 'knows', 'l', 'last', 'lately', 'later', 'latter', 'latterly', 'least', 'less', 'lest', 'let', "let's", 'like', 'liked', 'likely', 'likewise', 'little', 'look', 'looking', 'looks', 'low', 'lower', 'ltd', 'm', 'made', 'mainly', 'make', 'makes', 'many', 'may', 'maybe', "mayn't", 'me', 'mean', 'meantime', 'meanwhile', 'merely', 'might', "mightn't", 'mine', 'minus', 'miss', 'more', 'moreover', 'most', 'mostly', 'mr', 'mrs', 'much', 'must', "mustn't", 'my', 'myself', 'n', 'name', 'namely', 'nd', 'near', 'nearly', 'necessary', 'need', "needn't", 'needs', 'neither', 'never', 'neverf', 'neverless', 'nevertheless', 'new', 'next', 'nine', 'ninety', 'no', 'nobody', 'non', 'none', 'nonetheless', 'noone', 'no-one', 'nor', 'normally', 'not', 'nothing', 'notwithstanding', 'novel', 'now', 'nowhere', 'o', 'obviously', 'of', 'off', 'often', 'oh', 'ok', 'okay', 'old', 'on', 'once', 'one', 'ones', "one's", 'only', 'onto', 'opposite', 'or', 'other', 'others', 'otherwise', 'ought', "oughtn't", 'our', 'ours', 'ourselves', 'out', 'outside', 'over', 'overall', 'own', 'p', 'particular', 'particularly', 'past', 'per', 'perhaps', 'placed', 'please', 'plus', 'possible', 'presumably', 'probably', 'provided', 'provides', 'q', 'que', 'quite', 'qv', 'r', 'rather', 'rd', 're', 'really', 'reasonably', 'recent', 'recently', 'regarding', 'regardless', 'regards', 'relatively', 'respectively', 'right', 'round', 's', 'said', 'same', 'saw', 'say', 'saying', 'says', 'second', 'secondly', 'see', 'seeing', 'seem', 'seemed', 'seeming', 'seems', 'seen', 'self', 'selves', 'sensible', 'sent', 'serious', 'seriously', 'seven', 'several', 'shall', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'since', 'six', 'so', 'some', 'somebody', 'someday', 'somehow', 'someone', 'something', 'sometime', 'sometimes', 'somewhat', 'somewhere', 'soon', 'sorry', 'specified', 'specify', 'specifying', 'still', 'sub', 'such', 'sup', 'sure', 't', 'take', 'taken', 'taking', 'tell', 'tends', 'th', 'than', 'thank', 'thanks', 'thanx', 'that', "that'll", 'thats', "that's", "that've", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'thence', 'there', 'thereafter', 'thereby', "there'd", 'therefore', 'therein', "there'll", "there're", 'theres', "there's", 'thereupon', "there've", 'these', 'they', "they'd", "they'll", "they're", "they've", 'thing', 'things', 'think', 'third', 'thirty', 'this', 'thorough', 'thoroughly', 'those', 'though', 'three', 'through', 'throughout', 'thru', 'thus', 'till', 'to', 'together', 'too', 'took', 'toward', 'towards', 'tried', 'tries', 'truly', 'try', 'trying', "t's", 'twice', 'two', 'u', 'un', 'under', 'underneath', 'undoing', 'unfortunately', 'unless', 'unlike', 'unlikely', 'until', 'unto', 'up', 'upon', 'upwards', 'us', 'use', 'used', 'useful', 'uses', 'using', 'usually', 'v', 'value', 'various', 'versus', 'very', 'via', 'viz', 'vs', 'w', 'want', 'wants', 'was', "wasn't", 'way', 'we', "we'd", 'welcome', 'well', "we'll", 'went', 'were', "we're", "weren't", "we've", 'what', 'whatever', "what'll", "what's", "what've", 'when', 'whence', 'whenever', 'where', 'whereafter', 'whereas', 'whereby', 'wherein', "where's", 'whereupon', 'wherever', 'whether', 'which', 'whichever', 'while', 'whilst', 'whither', 'who', "who'd", 'whoever', 'whole', "who'll", 'whom', 'whomever', "who's", 'whose', 'why', 'will', 'willing', 'wish', 'with', 'within', 'without', 'wonder', "won't", 'would', "wouldn't", 'x', 'y', 'yes', 'yet', 'you', "you'd", "you'll", 'your', "you're", 'yours', 'yourself', 'yourselves', "you've", 'z', 'zero'];
 
 const searchList=productDetails.map(product=>{
     return{
-        value: product.productCategory.replace(/_/g,' ')+' - '+product.productName.replace(/_/g,' ').replace(/\|/g,', '),
+        value: product.productId,
         label: product.productCategory.replace(/_/g,' ')+' - '+product.productName.replace(/_/g,' ').replace(/\|/g,', ')
     }
 });
 
-export default class Search extends React.Component{
+class Search extends React.Component{
     constructor(){
         super();
         this.state={
-            search:null
+            search:null,
+            openMenu: false
         };
     }
-    searchSpace=(event)=>{
-        let keyword = event.target.value;
-        this.setState({search:keyword})
+    handleClick(){ 
+        if (this.state.search == null) return null;
+        else{
+            let keywordsList=this.state.search.split(' ');
+            let searchKeywordsList=[];
+            keywordsList.forEach(word => {
+                if (!skipWordsList.includes(word)) searchKeywordsList.push(word);            
+            });
+            let displayIds=[];
+            searchKeywordsList.forEach((keyword)=>{
+                productDetails.filter((product)=>{
+                    if (!displayIds.includes(product.productId)){
+                        if ((product.productName!=null && product.productName.replace(/_/g,' ').replace(/\|/g,' ').toLowerCase().includes(keyword.toLowerCase())) || 
+                        (product.productCategory!=null && product.productCategory.replace(/_/g,' ').toLowerCase().includes(keyword.toLowerCase())) || 
+                        (product.productSpecification!=null && product.productSpecification.toLowerCase().includes(keyword.toLowerCase())) || 
+                        (product.productDescription!=null && product.productDescription.toLowerCase().includes(keyword.toLowerCase())) || 
+                        (product.Features!=null && product.Features.toLowerCase().includes(keyword.toLowerCase()))){
+                            displayIds.push(product.productId);
+                        }
+                    }
+                        
+                })
+            })
+            console.log(displayIds);
+        }
     }
-    search(){
-        console.log(this.state.search);
+    handleInputChange = (query, { action }) => {
+        if (action === "input-change" && query.length>0) {
+            this.setState({ openMenu: true,search: query });
+        }
+    };
+    hideMenu = () => {
+        this.setState({ openMenu: false });
+    };
+    redirectToOption = (selectedOptionValue) => {
+        this.setState({openMenu:false});
+        const {history} = this.props;
+        let product=productDetails.find(productObj=>productObj.productId===selectedOptionValue.value);
+        let redirectUrl='../product/'+product.productCategory+'-'+product.productName;
+        history.push(redirectUrl);
     }
     render(){
-        /*
-        const items = productDetails.filter((product)=>{
-            if(this.state.search == null)
-                return null;
-            else if(product.productName.replace('_',' ').toLowerCase().includes(this.state.search.toLowerCase()) || product.productCategory.replace('_',' ').toLowerCase().includes(this.state.search.toLowerCase())){
-                return product
-            }
-          }).map(product=>{
-            return(
-            <div>
-              <ul>
-              <li>
-                    <span>{product.productName}</span>
-                    <span>{product.productCategory}</span>
-                </li>
-              </ul>
-            </div>
-            )
-          })
-          */
           const DropdownIndicator = props => {
             return (
               <components.DropdownIndicator {...props}>
@@ -55,27 +72,25 @@ export default class Search extends React.Component{
               </components.DropdownIndicator>
             );
           };
-          const onKeyDown = e => {
-              if (e.keyCode===13){
-                  this.searchSpace(e);
-                  this.search();
-                  alert(e.target.value)
-              }
-          }
-      
-        return(
-            <div style={{width:"300px"}}>
-                <Select placeholder="Search for products.." options={searchList} components={{DropdownIndicator}} onKeyDown={onKeyDown} />
-                {
-                    /*<form>
-                    <div className="searchBar">
-                    <SearchIcon fontSize={"large"} />
-                    <input type="text" placeholder="Search for Products.." onChange={(e)=>this.searchSpace(e)}/> 
-                    {items}
-                    </div>
-                </form>
-                */}
+            return(
+            <div className="searchBar">
+                <div className="selectSearch">
+                <Select id="keywordValue" 
+                    onInputChange={this.handleInputChange} 
+                    onChange={(selectedOption)=>{this.redirectToOption(selectedOption)}} 
+                    onBlur={this.hideMenu} 
+                    menuIsOpen={this.state.openMenu} 
+                    placeholder="Search for products.." 
+                    options={searchList} 
+                    components={{DropdownIndicator}} 
+                    noOptionsMessage={()=>null} />
+                </div>
+                <div>
+                <Button className="searchButton" onClick={this.handleClick.bind(this)}>Search</Button>
+                </div>
             </div>
         )
     }
 }
+
+export default withRouter(Search);
